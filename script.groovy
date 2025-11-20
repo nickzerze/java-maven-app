8d08b43f3aba11270351ace583372d002128347d
Nikolaos Zerzelidis <nickzerze@gmail.com>
2025-11-20 12:10:10 +0200

    Create script.groovy for Jenkinsfile-basic

--- script.groovy
+++ script.groovy
@@ -5,10 +5,11 @@ def buildJar() {
 
 def buildimage() {
     echo "Building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')])
-    sh 'docker build -t malware4/java-maven-app:jma-2.0 .'
-    sh 'echo $PASS | docker login -u $USER --password-stdin'
-    sh 'docker push malware4/java-maven-app:jma-2.0'
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t malware4/java-maven-app:jma-2.0 .'
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'docker push malware4/java-maven-app:jma-2.0'
+    }
 }
 
 
